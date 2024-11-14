ff16749c59323adb78e5bd981fc80d4379ea4b5b
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2024-11-14 19:50:20 +0100

    08 create channel

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -67,7 +67,9 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className=" px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem 
+            onClick={()=>onOpen("createChannel",{server})}
+            className=" px-3 py-2 text-sm cursor-pointer">
             Create Channel
             <PlusCircle className="size-4 ml-auto" />
           </DropdownMenuItem>
