4c67f55b1b4ac18a4458d0501d1d33dc1cd9f13e
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2024-11-15 16:24:06 +0100

    14 conversation setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
+import UserAvatar from "../user-avatar";
 
 interface ChatHeaderProps {
   serverId:string;
@@ -19,6 +20,9 @@ export const ChatHeader = ({
       <MobileToggle serverId={serverId}/>
       {type === "channel" && (
         <Hash className="size-5 text-zinc-500 darl:text-zinc-400 mr-2"/>
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="size-8 md:size-8 mr-2"/>
       )}
       <p className="font-semibold text-md text-black dark:text-white">
         {name}
